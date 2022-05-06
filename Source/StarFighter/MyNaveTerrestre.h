721d0341ed5223dec7dc30713ddd3e6fd819df50
Carlos Walter Pacheco Lora <cwpachecol@usfx.bo>
2022-05-05 20:46:24 -0400

    Solucion agregada

---
+++ Source/StarFighter/MyNaveTerrestre.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NaveTerrestre.h"
+#include "MyNaveTerrestre.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARFIGHTER_API AMyNaveTerrestre : public ANaveTerrestre
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/StarFighter/NaveTerrestreEnemiga01.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NaveTerrestre.h"
+#include "NaveTerrestreEnemiga01.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARFIGHTER_API ANaveTerrestreEnemiga01 : public ANaveTerrestre
+{
+	GENERATED_BODY()
+	
+};
